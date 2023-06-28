b5970a43cd2b281ceeed008d83aaba0f27533226
Hrishikesh M Rao <96303885+HrishikeshMRao@users.noreply.github.com>
2023-06-28 17:52:00 +0530

    added space,time complexity comments

--- Running Sum of 1d Array.java
+++ Running Sum of 1d Array.java
@@ -1,6 +1,6 @@
 //Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum of nums.
-
-
+//time complexity O(n)
+//space complexity O(1)
 
 class Solution {
     public int[] runningSum(int[] nums)
