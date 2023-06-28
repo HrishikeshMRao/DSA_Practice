4e6359197118a433f2fd772fc9d070822124782a
Hrishikesh M Rao <96303885+HrishikeshMRao@users.noreply.github.com>
2023-06-28 20:43:36 +0530

    changed time complexity

--- Number of Steps to Reduce a Number to Zero.java
+++ Number of Steps to Reduce a Number to Zero.java
@@ -3,7 +3,7 @@ Given an integer num, return the number of steps to reduce it to zero.
 
 In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 """  
-//time O(n)
+//time O(logn)
 //space O(1)  
 
 
