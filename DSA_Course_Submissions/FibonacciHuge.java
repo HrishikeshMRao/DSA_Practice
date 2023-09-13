47dd8476f2eb91b8a009318f4ccceb9938344b34
Hrishikesh M Rao <96303885+HrishikeshMRao@users.noreply.github.com>
2023-09-13 19:54:34 +0530

    huge

--- DSA_Course_Submissions/FibonacciHuge.java
+++ DSA_Course_Submissions/FibonacciHuge.java
@@ -1,21 +1,40 @@
 import java.util.Scanner;
 
 public class FibonacciHuge {
-    private static long long getFibonacciHugeNaive(long n, long m) {
+
+    private static long pisano(long m)
+    {
+        long prev = 0;
+        long curr = 1;
+        long res = 0;
+        for(int i = 0; i < m * m; i++)
+        {
+            long temp = curr;
+            curr = (prev + curr) % m;
+            prev = temp;
+            if (prev == 0 && curr == 1)
+                res= i + 1;
+        }
+        return res;
+    }
+
+    private static long getFibonacciHugeNaive(long n, long m) {
         
-        long long previous = 0;
-        long long digit = 1;
+        long pisanoPeriod = pisano(m);
+        n = n % pisanoPeriod;
+        long previous = 0;
+        long digit = 1;
 
         if (n <= 1)
             return n;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             long temp = digit;
-            digit = (previous + digit);
+            digit = (previous + digit)%m;
             previous = temp;
             
         }
-        return digit;
+        return digit%m;
     }
     
     public static void main(String[] args) {
